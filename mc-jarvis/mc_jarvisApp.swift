f9a58e0aa9d8df0c1a52b76c3af0b8ebda57c601
RoninJc <jesus.castanocandela@gmail.com>
2024-08-23 19:44:50 +0200

    Initial Commit

---
+++ mc-jarvis/mc_jarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  mc_jarvisApp.swift
+//  mc-jarvis
+//
+//  Created by Jesus Castano Candela on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct mc_jarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
